1b09c8d253245f598f22f87516080b6a542d3c09
Madhav Suresh <madhav@u.northwestern.edu>
2022-12-11 09:47:11 -0600

    output scores and error

--- banzhaf.py
+++ banzhaf.py
@@ -3,6 +3,8 @@ from sampling import sampler
 import performance_scores
 import numpy as np
 import sys
+from bundling import kmeans_cluster
+import error_measures
 
 ''' 
 this can be some idea of accuracy of the model. 
@@ -10,22 +12,39 @@ this can be some idea of accuracy of the model.
 input is some subset of the data
 '''
 
-def utility_function(data_subset, X, Y):
+def utility_function(data_subset, X, Y, x_test, y_test):
     xcut = X[data_subset, :]
     ycut = Y[data_subset,:]
+    s = sampler()
     if np.sum(s.classify(ycut.ravel())) in [len(s.classify(ycut.ravel())),0]:
         return 0
-    return performance_scores.logistics_MAE_score(xcut, s.classify(ycut.ravel()), xcut, ycut)
+    return performance_scores.logistics_MAE_score(xcut, s.classify(ycut.ravel()), x_test, y_test)
 
 def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
+
+def banzhaf_bundled_remove_all(subset, utility, N, X, Y, x_test, y_test):
+    r = list(range(0, N))
+    out_of_bundle = [i for i in r if i not in subset]
+    ps = powerset(out_of_bundle)
+    total_val = 0.0
+    for j in ps:
+        if len(j) < 4:
+            continue
+        without_i = list(j)
+        with_i = without_i + subset
+        utility_without = utility_function(without_i, X, Y, x_test, y_test)
+        utility_with = utility_function(with_i, X, Y, x_test, y_test)
+        total_val += (utility_with - utility_without)
+    return total_val
+
 '''
 N is the length of the dataset
 i is the index index into N
 '''
-def banzhaf(i, utility, N, X, Y):
+def banzhaf(i, utility, N, X, Y, x_test, y_test):
     r = chain(range(0,i) , range(i+1, N))
     ps = powerset(r)
     total_val = 0.0
@@ -34,10 +53,43 @@ def banzhaf(i, utility, N, X, Y):
             continue
         without_i = list(j)
         with_i = without_i + [i,]
-        utility_without = utility_function(without_i, X, Y)
-        utility_with = utility_function(with_i, X, Y)
+        utility_without = utility_function(without_i, X, Y, x_test, y_test)
+        utility_with = utility_function(with_i, X, Y, x_test, y_test)
         total_val += (utility_with - utility_without)
-    print(total_val)
+    return total_val
+
+
+
+def driver(number_samples, num_clusters):
+    N = number_samples
+    s = sampler()
+    xs = s.sample_X(N)
+    #split_x = xs.array_split(2)
+    x_train = s.sample_X(N)
+    y_train = s.sample_Y(x_train)
+    x_test =  s.sample_X(N)
+    y_test = s.sample_Y(x_test)
+    clusters = kmeans_cluster(x_train, k=num_clusters)
+    cluster_map = {}
+    for i in range(len(clusters)):
+        cluster_map.setdefault(clusters[i],[]).append(i)
+
+    #banzhaf(int(args[1]), utility_function, N, xs, ys)
+
+    bundled_measure =[] 
+    for i in range(num_clusters):
+        bundled_measure.append(banzhaf_bundled_remove_all(cluster_map[i], 
+                                     utility_function, N, x_train, 
+                                     y_train, x_test, y_test))
+    
+    print("======")
+    measure = []
+    for i in range(N):
+        measure.append(banzhaf(i, utility_function, N, x_train, y_train, x_test, y_test))
+
+    print(error_measures.bundle_difference(clusters, np.array(bundled_measure), np.array(measure), error_measures.abs_distance))
+
+    print(error_measures.individual_difference(clusters, np.array(bundled_measure), np.array(measure), error_measures.abs_distance))
 
 if __name__ == "__main__":
     args = sys.argv[1:]
@@ -45,7 +97,7 @@ if __name__ == "__main__":
         print("Expected usage:\n python3 banzhaf.py {num_samples} {index to value}")
         sys.exit(0)
     N = int(args[0])
-    s = sampler()
-    xs = s.sample_X(N)
-    ys = s.sample_Y(xs)
-    banzhaf(int(args[1]), utility_function, N, xs, ys)
+    num_clusters = int(args[1])
+    driver(N, num_clusters)
+
+
--- bundling.py
+++ bundling.py
@@ -6,6 +6,3 @@ returns partition function after clustering
 def kmeans_cluster(X, k=3):
     kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
     return kmeans.labels_
-
-
-
